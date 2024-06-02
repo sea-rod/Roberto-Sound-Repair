178b75188e02672ce9a230cfd83836486938b2ae
sea <sea11rod@gmail.com>
2024-06-02 20:46:31 +0530

    animation added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add("show");
+    else entry.target.classList.remove("show");
+  });
+});
+
+const hidden_elements = document.querySelectorAll(".hidden");
+hidden_elements.forEach((element) => {
+  observer.observe(element);
+});
